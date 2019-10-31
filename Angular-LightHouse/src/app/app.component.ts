fa1ebe40ef5337ceade918785db0dd6293680006
unknown <mcbride.mik@gmail.com>
2019-10-30 21:35:57 -0400

    Changing title

--- Angular-LightHouse/src/app/app.component.ts
+++ Angular-LightHouse/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-LightHouse';
+  title = 'Welcome to LightHouse';
 }
