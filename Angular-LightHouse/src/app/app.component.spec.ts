c6205f7a3484e2513e69d27d7fd3788d3ae6ac74
Robert Li <bobli2011@gmail.com>
2019-10-30 21:47:52 -0400

    added some unit tests for angular

--- Angular-LightHouse/src/app/app.component.spec.ts
+++ Angular-LightHouse/src/app/app.component.spec.ts
@@ -16,16 +16,23 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-LightHouse'`, () => {
+  it(`should have as title 'Welcome to LightHouse'`, async() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular-LightHouse');
+    expect(app.title).toEqual('Welcome to LightHouse');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular-LightHouse app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to LightHouse');
+  });
+
+  it('should render the logo', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('nav>img').src).toContain("assets/Revature%20Logo.png");
   });
 });
