9e6f6185b605ef7132a742ff63b7f035ff2eb4a4
unknown <mcbride.mik@gmail.com>
2019-10-30 21:48:33 -0400

    Editing name of Revature logo file for testability

--- Angular-LightHouse/src/app/app.module.ts
+++ Angular-LightHouse/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -13,6 +14,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
