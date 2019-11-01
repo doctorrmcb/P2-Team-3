857ce313d7495cc8d4fd2bcface8d1544316738b
Erik Haklar <erikhaklar@gmail.com>
2019-11-01 09:43:35 -0500

    Upload Files Done
    Can now upload files, but security credentials are removed from git upload with replace me text

--- LightHouse/src/main/java/com/revature/s3/FileManagementS3.java
+++ LightHouse/src/main/java/com/revature/s3/FileManagementS3.java
@@ -1,15 +1,39 @@
 package com.revature.s3;
 
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 import com.revature.dao.S3FileDAO;
 import com.revature.pojo.S3File;
 import static com.revature.util.LoggerUtil.*;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class FileManagementS3 implements S3FileDAO{
 	
-	private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+	AWSCredentials credentials = new BasicAWSCredentials(
+	  "replace me", 
+	  "replace me too"
+	);
+							
+	AmazonS3 s3client = AmazonS3ClientBuilder
+		  .standard()
+		  .withCredentials(new AWSStaticCredentialsProvider(credentials))
+		  .withRegion(Regions.US_EAST_2)
+		  .build();
+	
+	//private final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
 
 	/**
 	 * The UploadFile method uploads a specified file to the specified Amazon
@@ -28,19 +52,55 @@ public class FileManagementS3 implements S3FileDAO{
 	public boolean uploadFile(S3File file) {
 		
 		try {
-			s3.putObject(file.getBucketName(), file.getKeyName(), file.getFilePath());
+			s3client.putObject(file.getBucketName(), file.getKeyName(), file.getFilePath());
 		} catch (AmazonServiceException e) {
 			error(e.getErrorMessage());
+			//System.exit(1);
 			return false;
 		}
 		
 		return true;
 	}
-
+	
+	/**
+	 * The downloadFile method will get the file specified within the file given
+	 * to the method and write it to the client's specified file path on their
+	 * machine. Oppose to the UploadFile method, the file.getFilePath() now
+	 * specifies the path on the client's machine where the downloaded file
+	 * should go to.
+	 * 
+	 * @author Erik Haklar
+	 * @version 1.0
+	 * @since 2019-10-31
+	 */
 	@Override
-	public S3File downloadFile(S3File file) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean downloadFile(S3File file) {
+		
+		boolean gotFile = false;
+		S3Object object = s3client.getObject(new GetObjectRequest(file.getBucketName(), file.getKeyName()));
+		InputStream objectDataInput = object.getObjectContent();
+		//process the object data input stream
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(object.getObjectContent()));
+			
+			FileWriter writer = new FileWriter(file.getFilePath()); 
+			BufferedWriter bufferWriter = new BufferedWriter(writer);
+			
+			String line;
+			while((line = reader.readLine()) != null)
+			{
+				bufferWriter.write(line);
+				gotFile = true;
+			}
+			
+			objectDataInput.close();
+			bufferWriter.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return gotFile;
 	}
 
 	
