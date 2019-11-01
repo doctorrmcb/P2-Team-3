2ec942e24e22df2193aeeadb93604f9dc672ef0b
Robert Li <bobli2011@gmail.com>
2019-11-01 19:10:02 -0400

    testing daos

--- LightHouse/src/main/java/com/revature/driver/Driver.java
+++ LightHouse/src/main/java/com/revature/driver/Driver.java
@@ -10,5 +10,16 @@ public class Driver {
 		User user = userDAO.getUserByUsername("testUsername");
 		
 		System.out.println(user);
+		
+		User user2 = new User();
+		user2.setUserID(2);
+		user2.setUsername("test");
+		user2.setPassword("test");
+		user2.setEmailName("test");
+		user2.setRoles("associate");
+		user2.setFullName("test");
+		user2.setBatchID("test");
+		
+		userDAO.createUser(user2);
 	}
 }
