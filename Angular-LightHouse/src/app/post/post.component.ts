4a6d43071630b6853d9cd959ea5bbc7c1a4db776
unknown <mcbride.mik@gmail.com>
2019-11-06 13:41:21 -0500

    Formatting date/time output

--- Angular-LightHouse/src/app/post/post.component.ts
+++ Angular-LightHouse/src/app/post/post.component.ts
@@ -21,8 +21,29 @@ export class PostComponent implements OnInit {
     console.log(this.inputPost);
     this.contents = this.inputPost.contents;
     this.username = this.inputPost.posted_by.username;
-    this.postDate = this.inputPost.postDate[0].toString() + "/" + this.inputPost.postDate[1].toString() + "/" + this.inputPost.postDate[2].toString();
-    this.postTime = this.inputPost.postTime[0].toString() + ":" + this.inputPost.postTime[1];
+    this.postDate = this.inputPost.postDate[1].toString() + "/" + this.inputPost.postDate[2].toString() + "/" + this.inputPost.postDate[0].toString();
+    
+    let period = "AM";
+
+    if (this.inputPost.postTime[0] > 12) {
+      let hour = this.inputPost.postTime[0] - 12;
+      this.postTime = hour.toString()
+      period = "PM";
+    } else {
+      this.postTime = this.inputPost.postTime[0].toString()
+    }
+
+    this.postTime += ":";
+
+    if (this.inputPost.postTime[1] < 10) {
+      this.postTime += "0"
+      this.postTime += this.inputPost.postTime[1].toString();
+    } else {
+      this.postTime += this.inputPost.postTime[1].toString();
+    }
+
+    this.postTime += " " + period;
+
   }
 
 }
