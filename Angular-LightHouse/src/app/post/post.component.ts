49fd94357a9b9400c1b2d125bcc21443fcc049ca
unknown <mcbride.mik@gmail.com>
2019-11-06 12:58:26 -0500

    Adding some post TS

--- Angular-LightHouse/src/app/post/post.component.ts
+++ Angular-LightHouse/src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../types/Post';
 
 
@@ -9,7 +9,7 @@ import { Post } from '../types/Post';
 })
 export class PostComponent implements OnInit {
 
-  //@Input inputPost: Post;
+  @Input() inputPost: Post;
   constructor() { }
 
   ngOnInit() {
