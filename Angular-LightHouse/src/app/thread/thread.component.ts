39d9849859e23b6fa3dd13e6fb9a934e8cb10b08
unknown <mcbride.mik@gmail.com>
2019-11-06 11:57:49 -0500

    Thread update

--- Angular-LightHouse/src/app/thread/thread.component.ts
+++ Angular-LightHouse/src/app/thread/thread.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Post } from '../types/Post';
 
 @Component({
   selector: 'app-thread',
@@ -10,8 +11,9 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 export class ThreadComponent implements OnInit {
 
   id: string;
-  title: string;
- 
+  title = "";
+  posts: Post[];
+  //thread: Thread;
 
   constructor(private route: ActivatedRoute,
     private http: HttpClient) { }
@@ -19,6 +21,17 @@ export class ThreadComponent implements OnInit {
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     this.title = this.route.snapshot.paramMap.get('title');
+    //this.thread = this.route.snapshot.paramMap.get('thread');
+    this.getPosts();
+  }
+
+  getPosts() {
+    let url = 'http://localhost:8080/LightHouse/post/' + this.title;
+    let result = this.http.get<Post[]>(url, {}).subscribe(cr => {
+      this.posts = cr;
+        //alert("Authentication failed.");
+      
+    });
   }
 
 }
