f52c791267f6de1212a156b719bad74126e3d75f
unknown <mcbride.mik@gmail.com>
2019-11-06 13:13:33 -0500

    Changes to post component

--- Angular-LightHouse/src/app/post/post.component.ts
+++ Angular-LightHouse/src/app/post/post.component.ts
@@ -10,9 +10,18 @@ import { Post } from '../types/Post';
 export class PostComponent implements OnInit {
 
   @Input() inputPost: Post;
+  contents: string;
+  username: string;
+  postDate: string;
+  postTime: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.contents = this.inputPost.contents;
+    this.username = this.inputPost.postedBy.username;
+    this.postDate = this.inputPost.postDate[0].toString() + "/" + this.inputPost.postDate[1].toString() + "/" + this.inputPost.postDate[2].toString();
+    this.postTime = this.inputPost.postTime[0].toString() + ":" + this.inputPost.postTime[1];
   }
 
 }
