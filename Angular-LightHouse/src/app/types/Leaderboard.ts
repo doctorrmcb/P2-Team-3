00a31cca61b2dc27ea2321cd4a396871ddc4dd5e
Robert Li <bobli2011@gmail.com>
2019-11-06 23:26:31 -0500

    working on leaderboard, build html skeleton

---
+++ Angular-LightHouse/src/app/types/Leaderboard.ts
@@ -0,0 +1,12 @@
+import { Category } from './Category';
+
+export class Leaderboard{
+
+    username: string;
+    cat: Category;
+    quizScore: number;
+    timeTaken: number;
+
+    dateTaken: number[];
+
+}
