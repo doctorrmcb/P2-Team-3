8d1648f8766e2d4ba9bfb46c6acb9c67f62e11bf
unknown <mcbride.mik@gmail.com>
2019-11-06 19:51:07 -0500

    adding changes

--- Angular-LightHouse/src/app/app-routing.module.ts
+++ Angular-LightHouse/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { GeneralForumInnerComponent } from './general-forum-inner/general-forum-
 import { ThreadComponent } from './thread/thread.component';
 import { PostComponent } from './post/post.component';
 import { SubForumComponent } from './sub-forum/sub-forum.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 const appRoutes: Routes = [
   {path: '', component: LoginComponent},
@@ -18,7 +19,8 @@ const appRoutes: Routes = [
   {path: 'general-forum-inner', component: GeneralForumInnerComponent},
   {path: 'post', component: PostComponent},
   {path: 'sub-forum/:id', component: SubForumComponent},
-  {path: 'sub-forum/:id/:title', component: ThreadComponent}
+  {path: 'sub-forum/:id/:title', component: ThreadComponent},
+  {path: 'leaderboard', component: LeaderboardComponent}
 ]
 
 
