e01671820b0498e4b79404bbafa2e72908bc4b96
doriapameni <doriapameni@gmail.com>
2019-11-07 12:11:37 -0500

    adding question type to view-question ts module

--- Angular-LightHouse/src/app/view-question/view-question.component.ts
+++ Angular-LightHouse/src/app/view-question/view-question.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {Injectable} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Category } from '../types/Category';
+import { Question } from '../types/Question';
 
 @Component({
   selector: 'app-view-question',
   templateUrl: './view-question.component.html',
   styleUrls: ['./view-question.component.css']
 })
+
+@Injectable()
 export class ViewQuestionComponent implements OnInit {
 
-  constructor() { }
+  selected: string;
+  categories: Category[];
+  questions: Question[];
+  
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.getCategories();
+    this.getQuestions();
   }
 
+  getCategories(){
+    let url = 'http://localhost:8080/LightHouse/view-question/';
+    let result = this.http.get<Category[]>(url, {}).subscribe(cr =>{
+      this.categories = cr;
+    });
+  }
+
+  getQuestions(){
+    let url = 'http://localhost:8080/LightHouse/view-question/';
+    let result = this.http.get<Question[]>(url, {}).subscribe(cr =>{
+      this.questions = cr;
+    });
+  }
 }
